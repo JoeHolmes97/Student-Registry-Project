a358c1a2d3c070439fd1b24cca02f29f5385cf5b
Joseph Holmes <joseph.holmes97@yahoo.co.uk>
2023-03-06 14:00:19 +0000

    Put main program inside a function

--- Week 8 Project/Week_8_Project.py
+++ Week 8 Project/Week_8_Project.py
@@ -18,8 +18,6 @@ import Generic_Functions as GenFun
 def LineBreak(): # Function for printing a line break
     print("\n------------------------\n")
 
-
-
 def RegStudents(o_StudentList): # Function for registering students
 
     bContinue = True # Set bContinue to True
@@ -85,8 +83,6 @@ def RegStudents(o_StudentList): # Function for registering students
             print("Returning to previous menu")
             bContinue = False            
             return o_StudentList
-
-
                    
 def AssignStudents(o_StudentList, s_Classes): # Function for assigning a student to a class
 
@@ -147,45 +143,49 @@ LineBreak()
 
 # ---------------------------Main Program-----------------------------
 
-print("Welcome to the college registry menu\n")
+def MainProgram():
 
-# o_StudentList = [] # Create an empty list for the Student objects ----------------Replace next line of code with this one when testing is finished
+    print("Welcome to the college registry menu\n")
 
-o_StudentList = [Cls.Student("Joe", "Holmes", 25), Cls.Student("Jack", "Ryan", 33), Cls.Student("James", "Cameron", 44), Cls.Student("Jill", "Valentine", 30), Student("Lyse", "Hext", 26)]
-# Test data above ^
-s_Classes = ["Programming", "Maths", "English", "Pyhsics", "History"]
+    # o_StudentList = [] # Create an empty list for the Student objects ----------------Replace next line of code with this one when testing is finished
+
+    o_StudentList = [Cls.Student("Joe", "Holmes", 25), Cls.Student("Jack", "Ryan", 33), Cls.Student("James", "Cameron", 44), Cls.Student("Jill", "Valentine", 30), Student("Lyse", "Hext", 26)]
+    # Test data above ^
+    s_Classes = ["Programming", "Maths", "English", "Pyhsics", "History"]
 
 
-bContinue = True # Set up a variable for exiting a loop
+    bContinue = True # Set up a variable for exiting a loop
 
-while bContinue == True: # While bContinue is True, loop
+    while bContinue == True: # While bContinue is True, loop
   
-    print("\n----------------College Registry Main Menu----------------\n")
+        print("\n----------------College Registry Main Menu----------------\n")
 
-    s_MenuItems = ["Please select an option:", "Register student", "Assign a student to a class", "Take a register", "Exit the program"]
-# Creates a list of menu items to use, with the possibity of adding more
-    sUserInput = GenFun.Menu(s_MenuItems) # Run the function Menu with the list of menu items, and assign the returned value to sUserInput
-
-    LineBreak()
-
-    if sUserInput == "1": # If the user enters 1, do this
-    
-        RegStudents(o_StudentList) # Run the RegStudents() function, and appends the returned result to o_StudentList list
-        
-        GenFun.DisplayStudents(o_StudentList)
+        s_MenuItems = ["Please select an option:", "Register student", "Assign a student to a class", "Take a register", "Exit the program"]
+    # Creates a list of menu items to use, with the possibity of adding more
+        sUserInput = GenFun.Menu(s_MenuItems) # Run the function Menu with the list of menu items, and assign the returned value to sUserInput
 
         LineBreak()
+
+        if sUserInput == "1": # If the user enters 1, do this
+    
+            RegStudents(o_StudentList) # Run the RegStudents() function, and appends the returned result to o_StudentList list
+        
+            GenFun.DisplayStudents(o_StudentList)
+
+            LineBreak()
   
-    elif sUserInput == "2": # Assign a student to a class, not coded yet
+        elif sUserInput == "2": # Assign a student to a class, not coded yet
        
-        AssignStudents(o_StudentList, s_Classes)
+            AssignStudents(o_StudentList, s_Classes)
 
-    elif sUserInput == "3":
-        print("Coming soon 3") # Take a register, not coded yet
+        elif sUserInput == "3":
+            print("Coming soon 3") # Take a register, not coded yet
 
-    elif sUserInput == "4": # Option for exiting the program
-        print("Exiting the program")
-        bContinue = False # Set bContinue to false, exiting the program
+        elif sUserInput == "4": # Option for exiting the program
+            print("Exiting the program")
+            bContinue = False # Set bContinue to false, exiting the program
+
+Main() # Runs the main program function
 
 # ---------------------------Main Program-----------------------------
 
